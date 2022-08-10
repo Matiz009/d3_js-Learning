335730b1eb0c6896da2ca444ebf8cbeb4a6e2e40
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-11 01:08:09 +0500

    line chart, pie chart

--- barchart.js
+++ barchart.js
@@ -1,26 +1,31 @@
 // javascript
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-let svgWidth = 500,
-    svgHeight = 300,
-    barPadding = 5;
-let barWidth = svgWidth / dataset.length;
+var data = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-let svg = d3.select("svg").attr("width", svgWidth).attr("height", svgHeight);
+var svgWidth = 500,
+    svgHeight = 300;
 
-let barChart = svg
-    .selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("y", function(d) {
-        return svgHeight - d;
-    })
-    .attr("height", function(d) {
-        return d;
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("transform", function(d, i) {
-        var translate = [barWidth * i, 0];
-        return "translate(" + translate + ")";
-    });
+var svg = d3.select("svg").attr("width", svgWidth).attr("height", svgHeight);
+
+var xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, svgWidth]);
+
+var yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([svgHeight, 0]);
+
+var x_axis = d3.axisBottom().scale(xScale);
+
+var y_axis = d3.axisLeft().scale(yScale);
+
+svg.append("g").attr("transform", "translate(50, 10)").call(y_axis);
+
+var xAxisTranslate = svgHeight - 20;
+
+svg
+    .append("g")
+    .attr("transform", "translate(50, " + xAxisTranslate + ")")
+    .call(x_axis);
